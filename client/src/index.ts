07173ee1bd310b843e07b546336a14001ab89925
Rafal <rafal.malewicz1@gmail.com>
2022-09-06 23:25:55 +0100

    dsfsf

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,12 @@
-console.log("tibzaaaaaaaaaazzzzzzzzzzzzzzz11111111111");
+import { io } from "socket.io-client";
+
+const socket = io("localhost:5000");
+
+setInterval(() => {
+  const start = Date.now();
+
+  socket.emit("ping", () => {
+    const duration = Date.now() - start;
+    console.log(duration + "ms");
+  });
+}, 1000);
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,20 @@
-console.log("server!!eeeeeeeeeeeeeeee");
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket: any) => {
+  socket.on("ping", (callback: any) => {
+    callback();
+  });
+});
+
+httpServer.listen(5000, () => console.log("listening"));
